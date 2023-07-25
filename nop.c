929f958391fb119459a97d3fab2d72c83c47199a
B2ma <beatrice.terer@gmail.com>
2023-07-25 13:20:30 +0000

    Created nop.c

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+* nop - does nothing
+* @stack: double pointer to stack
+* @line_number: the position where the instruction appears
+*/
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
