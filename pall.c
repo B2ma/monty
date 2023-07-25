00b4a426f4527c866a6c1f463b1d8ba8361c0175
Cassandra <cassandralelei013@gmail.com>
2023-07-25 04:48:28 -0700

    pall.c added

---
+++ pall.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * pall - prints all the values in stack starting
+ * from the top
+ * @stack: double pointer to top of the stack
+ * @line_number: shows the number where the instruction appears.
+ */
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+(void)line_number; 
+stack_t *current = *stack;
+while (current != NULL)
+{
+printf("%d\n", current->n);
+current = current->next;
+}
+}
