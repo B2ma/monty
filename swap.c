8ddc08dba12971120cb265eeb8892aad16f0633b
B2ma <beatrice.terer@gmail.com>
2023-07-25 12:10:11 +0000

    Created swap.c

---
+++ swap.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+/**
+* swap - swaps the top two elements of the stack
+* @stack: double pointer to stack
+* @line_number: the line where the instruction appears
+*/
+void swap(stack_t **stack, unsigned int line_number)
+{
+if (*stack == NULL || (*stack)->next == NULL)
+{
+fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+exit(EXIT_FAILURE);
+}
+
+int temp = (*stack)->n;
+(*stack)->n = (*stack)->next->n;
+(*stack)->next->n = temp;
+}
